3719eedefb0fe57af9deb79d2d38be4dcd9d4e79
sarker24 <sarker24@gmail.com>
2023-08-04 13:04:02 +0200

    integrate tailwind and router

---
+++ src/views/MoviesView.vue
@@ -0,0 +1,14 @@
+
+<script setup>
+   
+</script>
+
+<template>
+    <div>
+     this is movie page
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+
+<script setup>
+   
+</script>
+
+
+<template>
+    <div>
+      this is not found page
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
